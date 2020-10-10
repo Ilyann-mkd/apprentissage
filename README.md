f76630428adad835ad698df0ec50f82b60f88fa9
Ilyann-mkd <ily4nn.mekdad@gmail.com>
2020-10-10 10:18:48 +0000

    fix

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 
-Pour démarrer Gitpod avec le workspace, ouvrir la page:
-https://gitpod.io/#https://github.com/Ilyann-mkd/apprentissage
+Pour démarrer Gitpod avec le workspace, ouvrir la page:  
+https://gitpod.io/#https://github.com/Ilyann-mkd/apprentissage  
 
 
-Pour installer les dépendances du projet
-`> npm install
+Pour installer les dépendances du projet  
+ `npm install`  
 
-Pour exécuter le code
-`> node index.js
-`> node input.js
+Pour exécuter le code  
+ `node index.js`  
+ `node input.js`  
--- input.js
+++ input.js
@@ -4,3 +4,22 @@ var prompt = require('prompt-sync')();
 var name = prompt('Comment tu t\'appelles ?          ');
 console.log('Bonjour à toi: '+name);
 
+/* 
+Présentation + recup des infos des deux users 
+Affiche la grille vide 
+Phase de placement des 3 pions/personne chacun leur tour + vérification alignement
+Phase de déplacement des pions chacun leur tour + vérification alignement
+fin du jeu lorsque trois pions alignés
+
+Tant que personne n'a gagné 
+    Prochain tour de jeu
+    Vérification de l'alignement
+
+
+*/
+
+
+console.log("*****************************************************************************")
+console.log("Organisation des dialogues avec les utilisateurs,pour représenter les tours de jeu")
+
+
